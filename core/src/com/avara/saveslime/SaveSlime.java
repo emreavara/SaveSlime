66cbf7baff9d35482ee8d64bb7f5777fbb50f183
Avara, Emre <emre_avara@hotmail.com>
2021-04-30 01:40:28 +0300

    Add pictures and initialize variables

--- core/src/com/avara/saveslime/SaveSlime.java
+++ core/src/com/avara/saveslime/SaveSlime.java
@@ -1,31 +1,85 @@
 package com.avara.saveslime;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SaveSlime extends ApplicationAdapter {
+
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture slime;
+	Texture enemySlime;
+	Texture background;
+
+	// Slime
+	float slimeX = 0;
+	float slimeY = 0;
+	float slimeWidth;
+	float slimeHeight;
+
+	// Screen
+	int screenWidth;
+	int screenHeight;
+
+	// Game
+	public enum GameState {
+		idle,
+		playing,
+		gameOver
+	}
+	GameState gameState;
+
+
+
 	@Override
 	public void create () {
+
+
+		// Initialize
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		slime           = new Texture("slime.png");
+		enemySlime      = new Texture("enemy.png");
+		background      = new Texture("background.png");
+
+		// Screen
+		screenWidth     = Gdx.graphics.getWidth();
+		screenHeight    = Gdx.graphics.getHeight();
+
+		// Slime
+		slimeX          = screenWidth/4;
+		slimeY          = screenHeight/2;
+		slimeWidth      = screenWidth/10;
+		slimeHeight     = screenHeight/5;
+
+		// Game
+		gameState = GameState.idle;
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background,0,0, screenWidth, screenHeight);
+
+		switch (gameState){
+			case idle:
+				break;
+			case playing:
+				break;
+			case gameOver:
+				break;
+		}
+		batch.draw(slime, slimeX, slimeY, slimeWidth, slimeHeight);
+
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
